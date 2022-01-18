6f214740436c7c444ed7bcbb8475783281bc865b
Helba <Toyz@users.noreply.github.com>
2022-01-17 21:27:50 -0800

    Update Server.cs

--- seeder/Server.cs
+++ seeder/Server.cs
@@ -1,4 +1,8 @@
-﻿using MonoTorrent;
+// Licensed to the Rapture Contributors under one or more agreements.
+// The Rapture Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using MonoTorrent;
 using MonoTorrent.Client;
 using MonoTorrent.TorrentWatcher;
 
